fd8076b1e0f8629164f11fcfe5255c1aafece176
Felix Wollmann <50966625+F3l1xxx@users.noreply.github.com>
2022-02-01 22:22:51 +0100

    Added a little bit of documentation to CubitBase

--- lib/cubits/CubitBase.dart
+++ lib/cubits/CubitBase.dart
@@ -3,5 +3,12 @@ abstract class CubitBase<T> {
   Future<void> reload();
   Future<void> remove(T item);
   Future<T> create(T item);
+
+  /// Change the element [item], from its old state, to its new state.
+  /// 
+  /// The old state is identified by its id.
+  /// 
+  /// Returns a future with completes fine, when the element could be correctly edited & saved to the database.
+  /// Return a future with an error, when an error occured (network, database, ...).
   Future<void> change(T item);
 }
