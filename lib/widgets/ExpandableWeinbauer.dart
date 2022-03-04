c18fcd66a5d8349889746f267b24461d95060c48
Felix Wollmann <50966625+F3l1xxx@users.noreply.github.com>
2022-03-04 19:19:22 +0100

    Added ExpandableWeinbauer-Widget

---
+++ lib/widgets/ExpandableWeinbauer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExpandableWeinbauer extends StatefulWidget {
+  const ExpandableWeinbauer({ Key? key }) : super(key: key);
+
+  @override
+  State<ExpandableWeinbauer> createState() => _ExpandableWeinbauerState();
+}
+
+class _ExpandableWeinbauerState extends State<ExpandableWeinbauer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
